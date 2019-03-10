07fa7d00ddc692a11588b338d556192d4d6fd950
alimo13 <aliyamohamed13@gmail.com>
2019-03-09 16:04:02 -0800

    attempt push branch

---
+++ src/model/Task.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Task {
+    double targetHours;
+    double hoursSoFar;
+    boolean isComplete;
+    AbstractFigure figure;
+    private String name;
+
+    public Task(String name, double targetHours) {
+        this.name = name;
+        this.targetHours = targetHours;
+        hoursSoFar = 0;
+        isComplete = false;
+    }
+
+    public double getTargetHours() {
+        return targetHours;
+    }
+
+    public void setTargetHours(double targetHours) {
+        this.targetHours = targetHours;
+    }
+
+    public double getHoursSoFar() {
+        return hoursSoFar;
+    }
+
+    public void setHoursSoFar(double hoursSoFar) {
+        this.hoursSoFar = hoursSoFar;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean taskComplete() {
+        //call celebration
+        return (hoursSoFar >= targetHours);
+    }
+}
