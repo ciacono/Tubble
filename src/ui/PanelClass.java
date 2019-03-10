3b569e972a387c7ccfc19aa7b4b30942aa06710e
ciacono <claire.iacono@gmail.com>
2019-03-10 11:51:34 -0700

    bigger circle

--- src/ui/PanelClass.java
+++ src/ui/PanelClass.java
@@ -6,12 +6,12 @@ import java.awt.*;
 public class PanelClass extends JPanel {
 
     public PanelClass() {
-        setSize(200, 200);
+        setSize(500, 500);
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillOval(50,50,50,50);
-        g.setColor(Color.BLUE);
+        g.fillOval(50,50,400,400);
+        g.setColor(Color.cyan);
         }
     }
