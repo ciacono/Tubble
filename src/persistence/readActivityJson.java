7eac226e23192e6e9b02934d4363e4100070e64a
ciacono <claire.iacono@gmail.com>
2019-03-10 09:40:31 -0700

    persistence files added to git

---
+++ src/persistence/readActivityJson.java
@@ -0,0 +1,28 @@
+package persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import model.Activity;
+import model.Task;
+
+import java.io.*;
+
+public class readActivityJson {
+
+    public readActivityJson() {
+    }
+
+    public Activity readActivityJsonStream(InputStream in) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader( in, "UTF-8"));
+        Gson gson = new Gson();
+        Activity activity = new Activity();
+        reader.beginArray();
+        while (reader.hasNext()) {
+            Task task = gson.fromJson(reader, Task.class);
+            activity.addTask(task);
+        }
+        reader.endArray();
+        reader.close();
+        return activity;
+    }
+}
