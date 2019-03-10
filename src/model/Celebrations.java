7b5ca1bb1f0f186277f2a2979524de9dbd0a8f5c
Yuree Jang <yj.yuree.jang@gmail.com>
2019-03-09 16:55:11 -0800

    Added methods for Celebrations.

--- src/model/Celebrations.java
+++ src/model/Celebrations.java
@@ -1,7 +1,30 @@
 package model;
 
+import java.util.*;
+
 public class Celebrations {
 
+    private List<String> phrases;
+
     public Celebrations() {
+        phrases = new ArrayList<>();
+    }
+
+    public void addPhrase(String phrase) {
+        if (!this.phrases.contains(phrase))
+        this.phrases.add(phrase);
+    }
+
+    public void removePhrase(String phrase) {
+        if (this.phrases.contains(phrase)) {
+            this.phrases.remove(phrase);
+        }
+    }
+
+    public String celebrate() {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(this.phrases.size());
+        String randomPhrase = this.phrases.get(randomIndex);
+        return randomPhrase;
     }
 }
