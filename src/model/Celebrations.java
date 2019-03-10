6c5c42cffec7f8b1ce1c005e3c98d51420e52492
Yuree Jang <yj.yuree.jang@gmail.com>
2019-03-09 17:34:29 -0800

    Completed Celebrations implementations.

--- src/model/Celebrations.java
+++ src/model/Celebrations.java
@@ -12,7 +12,7 @@ public class Celebrations {
 
     public void addPhrase(String phrase) {
         if (!this.phrases.contains(phrase))
-        this.phrases.add(phrase);
+            this.phrases.add(phrase);
     }
 
     public void removePhrase(String phrase) {
@@ -22,9 +22,25 @@ public class Celebrations {
     }
 
     public String celebrate() {
+        buildPhrases();
         Random rand = new Random();
         int randomIndex = rand.nextInt(this.phrases.size());
         String randomPhrase = this.phrases.get(randomIndex);
         return randomPhrase;
+    }
+
+    private void buildPhrases() {
+        addPhrase("Celebrate yourself");
+        addPhrase("Time for a snack, you earned it");
+        addPhrase("Now you can YouTube surf without guilt");
+        addPhrase("Yaaas");
+        addPhrase("You're so on top of it");
+        addPhrase("Time for a bubble bath");
+        addPhrase("You deserve to eat cheese and cured meats");
+        addPhrase("Thank you, next");
+        addPhrase("Say bye felicia to that task");
+        addPhrase("Productivity beast, you are");
+        addPhrase("Limits do not apply to you. Keep going");
+        addPhrase("You got this");
     }
 }
